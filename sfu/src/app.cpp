a6137d4dfea67fbaa1739ff87592a5257e483256
jackie.ou <jackie.ou@ringcentral.com>
2023-11-15 13:12:53 +0800

    run sfu as deamon

--- sfu/src/app.cpp
+++ sfu/src/app.cpp
@@ -7,6 +7,7 @@
 * @CreateTime: 2023-11-1
 *************************************************************************/
 
+#include <unistd.h>
 #include <iostream>
 #include <atomic>
 #include "engine.h"
@@ -16,6 +17,67 @@
 #include "app_component.hpp"
 #include "oatpp/network/Server.hpp"
 #include "oatpp/web/server/HttpRouter.hpp"
+
+int32_t writePidFile(const std::string& pidFile)
+{
+    // -rw-r--r--
+    // 644
+    int mode = S_IRUSR | S_IWUSR |  S_IRGRP | S_IROTH;
+
+    int fd;
+    // open pid file
+    if ((fd = ::open(pidFile.c_str(), O_WRONLY | O_CREAT, mode)) == -1) {
+        SRV_LOGE("open pid file: %s", pidFile.c_str());
+        return -1;
+    }
+
+    // require write lock
+    struct flock lock;
+
+    lock.l_type = F_WRLCK; // F_RDLCK, F_WRLCK, F_UNLCK
+    lock.l_start = 0; // type offset, relative to l_whence
+    lock.l_whence = SEEK_SET;  // SEEK_SET, SEEK_CUR, SEEK_END
+    lock.l_len = 0;
+
+    if (fcntl(fd, F_SETLK, &lock) == -1) {
+        if (errno == EACCES || errno == EAGAIN) {
+            ::close(fd);
+            SRV_LOGE("srs is already running");
+            return -1;
+        }
+        SRV_LOGE("access to pid file: %s", pidFile.c_str());
+        return -1;
+    }
+
+    // truncate file
+    if (ftruncate(fd, 0) != 0) {
+        SRV_LOGE("truncate pid file: %s", pidFile.c_str());
+        return -1;
+    }
+
+    // write the pid
+    std::string pid = std::to_string(getpid());
+    if (write(fd, pid.c_str(), pid.length()) != (int)pid.length()) {
+        SRV_LOGE("write pid: %s to file: %s", pid.c_str(), pidFile.c_str());
+        return -1;
+    }
+
+    // auto close when fork child process.
+    int val;
+    if ((val = fcntl(fd, F_GETFD, 0)) < 0) {
+        SRV_LOGE("fcntl fd: %d", fd);
+        return -1;
+    }
+    val |= FD_CLOEXEC;
+    if (fcntl(fd, F_SETFD, val) < 0) {
+        SRV_LOGE("lock file: %s fd: %d", pidFile.c_str(), fd);
+        return -1;
+    }
+
+    SRV_LOGD("write pid: %s to %s success!", pid.c_str(), pidFile.c_str());
+
+    return 0;
+}
 
 void run(const oatpp::base::CommandLineArguments& cmdArgs)
 {
@@ -78,17 +140,60 @@ void run(const oatpp::base::CommandLineArguments& cmdArgs)
 
 int main(int argc, const char * argv[])
 {
+    bool runAsDeamon = false;
+    
     oatpp::base::CommandLineArguments cmdArgs(argc, argv);
     
-    if (!cmdArgs.hasArgument("--conf")) {
-        SRV_LOGE("Configuration file must be provided.\neg: ./sfu --conf path-to-file/config.json");
-        return -1;
+    if (cmdArgs.hasArgument("--deamon")) {
+        SRV_LOGE("run as deamon");
+        runAsDeamon = true;
     }
-    std::string configFile = cmdArgs.getNamedArgumentValue("--conf", "");
+    
+    if (runAsDeamon) {
+        int32_t pid = fork();
+        
+        if(pid < 0) {
+            SRV_LOGE("fork father process");
+            return -1;
+        }
+        
+        // grandpa
+        if(pid > 0) {
+            int status = 0;
+            waitpid(pid, &status, 0);
+            SRV_LOGD("grandpa process exit.");
+            exit(0);
+        }
+        
+        // father
+        pid = fork();
+        
+        if(pid < 0) {
+            SRV_LOGE("fork child process");
+            return -1;
+        }
+        
+        if(pid > 0) {
+            SRV_LOGD("father process exit");
+            exit(0);
+        }
+        
+        // son
+        SRV_LOGD("son(daemon) process running.");
+        
+        std::string pidFile("/usr/local/sfu/bin/sfu.pid");
+        writePidFile("pidFile");
+    }
     
     //MSEngine->init("/home/ubuntu/dev/mediasoup-server/install/conf/config.json");
     
     //MSEngine->init("/Users/jackie.ou/Desktop/Research/dev/mediasoup-server/install/conf/config.json");
+    
+    if (!cmdArgs.hasArgument("--conf")) {
+        SRV_LOGE("configuration file must be provided.\neg: ./sfu --conf path/config.json");
+        return -1;
+    }
+    std::string configFile = cmdArgs.getNamedArgumentValue("--conf", "");
     
     MSEngine->init(configFile);
     

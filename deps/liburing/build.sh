89ea095510ec44792d32aab1e589ef6f823095e9
jackie.ou <jackie.ou@ringcentral.com>
2023-12-11 16:53:47 +0800

    add liburing

---
+++ deps/liburing/build.sh
@@ -0,0 +1,16 @@
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+parent_dir=$(dirname $(pwd))
+main_dir=$(dirname ${parent_dir})
+
+echo "cur_dir = ${cur_dir}"
+echo "parent_dir = ${parent_dir}"
+echo "main_dir = ${main_dir}"
+
+/bin/rm -rf build
+
+chmod 777 config
+
+./configure --prefix=${main_dir}/release
+
+make -j4
+make install
